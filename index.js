4d77ecb2e74e83517e656d19a1148997c756301e
mamuner rashid <salmanmamun96@gmail.com>
2021-10-28 17:30:46 +0600

    added hellow api

--- index.js
+++ index.js
@@ -81,7 +81,36 @@ app.get('/', (req, res) => {
     res.send('Running Geneous server');
 });
 
+app.get('/hello', (req, res) => {
+    res.send('hello updated here')
+})
+
 
 app.listen(port, () => {
     console.log('runnning Geneous server on Port', port);
 })
+
+
+/*
+one time:
+1.heroku account open
+2.heroku software install
+
+Every projects
+1.git init
+2. .gitignore(node_modual,.env)
+3. push everything to git
+4.make sure you have this script: "start":"node index.js",
+5.moke sure: put process.env.PORT in front of your port number
+6.heroku login
+7.heroku create (only one time for a project)
+8.command:git push heroku main
+
+-----
+update
+1. save everything check locally
+2.git add,git commit -m "hellow" git push.
+3.git push heroku main
+
+
+*/
